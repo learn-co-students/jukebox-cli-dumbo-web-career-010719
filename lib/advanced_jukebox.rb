fb490f8d8b3ded4d9b7955e40ada498e7a12c200
Mohammad Ali <ali.mohammed200@gmail.com>
2018-12-25 04:36:39 +0000

    Done.

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -3,25 +3,32 @@
 #with the correct path to this directory on your computer
 
 # my_songs = {
-# "Go Go GO" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/01.mp3',
-# "LiberTeens" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/02.mp3',
-# "Hamburg" =>  '< path to this directory >/jukebox-cli/audio/Emerald-Park/03.mp3',
-# "Guiding Light" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/04.mp3',
-# "Wolf" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/05.mp3',
-# "Blue" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/06.mp3',
-# "Graduation Failed" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/07.mp3'
+# "Go Go GO" => '/home/exuberant-parser-1642/jukebox-cli-dumbo-web-career-010719/audio/Emerald-Park/01.mp3',
+# "LiberTeens" => '/home/exuberant-parser-1642/jukebox-cli-dumbo-web-career-010719/audio/Emerald-Park/02.mp3',
+# "Hamburg" =>  '/home/exuberant-parser-1642/jukebox-cli-dumbo-web-career-010719/audio/Emerald-Park/03.mp3',
+# "Guiding Light" => '/home/exuberant-parser-1642/jukebox-cli-dumbo-web-career-010719/audio/Emerald-Park/04.mp3',
+# "Wolf" => '/home/exuberant-parser-1642/jukebox-cli-dumbo-web-career-010719/audio/Emerald-Park/05.mp3',
+# "Blue" => '/home/exuberant-parser-1642/jukebox-cli-dumbo-web-career-010719/audio/Emerald-Park/06.mp3',
+# "Graduation Failed" => '/home/exuberant-parser-1642/jukebox-cli-dumbo-web-career-010719/audio/Emerald-Park/07.mp3'
 # }
 
 def help
-  #this method should be the same as in jukebox.rb
-
+  puts "help"
+  puts "list"
+  puts "play"
+  puts "exit"
 end
 
-
+# def list(songArr)
+#   songArr.each.with_index { |elem, i|
+#     puts "#{i+1}. #{elem}"
+#   }
+# end
 
 def list(my_songs)
   #this method is different! Collect the keys of the my_songs hash and 
   #list the songs by name
+  my_songs.each.with_index { |(k, v), i| puts "#{i+1}. #{k}"}
 end
 
 
@@ -34,12 +41,36 @@ def play(my_songs)
   #if it is, play the song using the system 'open <file path>' syntax
   #get the file path of the song by looking it up in the my_songs hash
   
+  puts "Please enter a song name:"
+  inp = gets.chomp
+  if my_songs.has_key?(inp)
+    puts "Now playing #{inp}"
+    system "open #{my_songs[inp]}"
+  else
+    puts "Invalid input, please try again"
+  end
+  
 end
 
 def exit_jukebox
-  #this method is the same as in jukebox.rb
+  puts "Goodbye"
 end
 
-def run(my_songs)
-  #this method is the same as in jukebox.rb
+def run(songArr)
+  help
+  userInp = ""
+  while userInp != "exit"
+    puts "Please enter a command:"
+    userInp = gets.chomp
+    case userInp
+    when "list"
+      list(songArr)
+    when "help"
+      help
+    when "play"
+      play(songArr)
+    when "exit"
+      exit_jukebox
+    end
+  end
 end
--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,51 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help
+  puts "help"
+  puts "list"
+  puts "play"
+  puts "exit"
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  inp = gets.chomp
+  if inp.length < 2 && inp.to_i.between?(1, 9)
+    puts "#{songs[inp.to_i - 1]}"
+  elsif songs.include?(inp)
+    puts inp
+  else
+    puts "Invalid input, please try again"
+  end
+end
+
+def list(songArr)
+  songArr.each.with_index { |elem, i|
+    puts "#{i+1}. #{elem}"
+  }
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songArr)
+  help
+  userInp = ""
+  while userInp != "exit"
+    puts "Please enter a command:"
+    userInp = gets.chomp
+    case userInp
+    when "list"
+      list(songArr)
+    when "help"
+      help
+    when "play"
+      play(songArr)
+    when "exit"
+      exit_jukebox
+    end
+  end
+
+end
