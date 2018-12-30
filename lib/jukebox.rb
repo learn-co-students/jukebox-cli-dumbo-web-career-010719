acbde8a4cdc2b6fc589094df4a9a3d32c46b8b47
github username <github email address>
2018-12-30 16:10:02 -0500

    updated phrase in play

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -27,7 +27,7 @@ def play(songs)
   puts "Please enter a song name or number:"
   user_input = gets.chomp
     if (1..9).to_a.include?(user_input.to_i)
-      puts "Plays #{songs[user_input.to_i-1]}"
+      puts "Playing #{songs[user_input.to_i-1]}"
     elsif songs.include?(user_input)
       puts "Playing #{user_input}"
     else
